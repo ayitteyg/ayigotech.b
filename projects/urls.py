dea705749319a6eb2890586515341cc99de666c6
ayitteyg <ayitteyg@yahoo.com>
2025-09-17 19:10:02 +0000

    add contact message

--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ContactMessageCreateView, ProjectViewSet
+from .views import ContactMessageViewSet, ProjectViewSet
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path("api/contact/", ContactMessageCreateView.as_view(), name="contact-create"),
+    path("api/contact/", ContactMessageViewSet.as_view(), name="contact-create"),
 ]
